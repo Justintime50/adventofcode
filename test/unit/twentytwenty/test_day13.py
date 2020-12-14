2ca615e90c41bdaaae1e5efe1901241b8c7a758d
Justintime50 <justinpaulhammond@gmail.com>
2020-12-13 23:34:12 -0700

    Adding day13 part 1

---
+++ test/unit/twentytwenty/test_day13.py
@@ -0,0 +1,10 @@
+from adventofcode.twentytwenty.day13 import (
+    get_data,
+    iterate_bus_ids,
+)
+
+
+def test_iterate_bus_ids():
+    timestamp, bus_ids = get_data()
+    answer = iterate_bus_ids(timestamp, bus_ids)
+    assert answer == 6568
