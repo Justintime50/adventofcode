642a15a76cdea479ba18795f7c813ee5acd5a829
Justintime50 <justinpaulhammond@gmail.com>
2021-02-03 00:04:44 -0700

    Replace Travis CI with GitHub Actions

--- setup.py
+++ setup.py
@@ -3,14 +3,10 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-# REQUIREMENTS = [
-#     'PyGithub >= 1.51',
-# ]
-
 setuptools.setup(
     name='adventofcode',
     version='0.1.0',
-    description='An Advent calendar of coding challenges.',  # noqa
+    description='An Advent calendar of coding challenges.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='http://github.com/justintime50/adventofcode',
@@ -22,7 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    # install_requires=REQUIREMENTS,
     extras_require={
         'dev': [
             'pytest >= 6.0.0',
