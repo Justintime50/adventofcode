9a263899ac3cbd7156a8968618b10899e2526e2a
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-12-03 00:56:08 -0700

    feat: add test template

--- setup_day.sh
+++ setup_day.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 main() {
-    local local_dir="adventofcode/_$1/$2"
-    echo "Setting up $local_dir..."
+    local local_dir
+    local_dir="adventofcode/_$1/$2"
+    local test_dir
+    test_dir="test/unit/_$1"
 
+    echo "Setting up $local_dir..."
     mkdir -p "$local_dir"
     echo "TODO: Add data here" >"$local_dir"/input.txt
     echo "TODO: Add data here" >"$local_dir"/prompt.txt
     echo "TODO: Add data here" >"$local_dir"/sample.txt
     cp adventofcode/challenge_template.py "$local_dir"/challenge.py
+    cp test/unit/test_template.py "$test_dir"/test_"$2".py
 }
 
 main "$@"
