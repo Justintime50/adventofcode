a325c312f2585ad4b999f0d4baae3ed85388b87e
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2021-12-06 23:30:06 -0700

    fix: tests day7 part1 2021

--- adventofcode/_2021/day7/challenge.py
+++ adventofcode/_2021/day7/challenge.py
@@ -1,38 +1,46 @@
-from collections import Counter
 from statistics import median
 
 from adventofcode.utils import open_input
 
 
 def main():
-    data = open_input('adventofcode/_2021/day7/sample.txt')
-    answer_1 = some_function(data)
-    # answer_2 = some_function(data)
-
-    print(answer_1, answer_2)
-
-    return answer_1, answer_2
-
-
-def some_function(data):
+    data = open_input('adventofcode/_2021/day7/input.txt')
     split_data = [int(crab) for crab in data[0].split(',')]
-    median_number = int(median(split_data))
-    # print(median_number)
 
+    best_number = find_median(split_data)
+    answer_1 = calculate_fuel(split_data, best_number)
+
+    print(answer_1)
+
+    return answer_1
+
+
+def find_median(data) -> int:
+    median_number = int(median(data))
+
+    return median_number
+
+
+def calculate_fuel(data, best_number: int):
+    """Found out later that Python has `abs()` to get the absolute value so no
+    need for the less than or greater than bit which would simplify this a lot.
+    """
     total_fuel = 0
-    for crab_position in split_data:
-        if crab_position > median_number:
-            fuel = crab_position - median_number
+    for crab_position in data:
+        if crab_position > best_number:
+            fuel = crab_position - best_number
             print(fuel)
-        elif crab_position < median_number:
-            fuel = median_number - crab_position
+        elif crab_position < best_number:
+            fuel = best_number - crab_position
             print(fuel)
-        elif crab_position == median_number:
+        elif crab_position == best_number:
             fuel = 0
 
         total_fuel += fuel
 
     print(total_fuel)
+
+    return total_fuel
 
 
 if __name__ == '__main__':
