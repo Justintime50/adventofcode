98e546d5342495e5d159be772e41d19fefe6ae8c
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2023-12-03 00:02:49 -0700

    fix: day 1 part 2 2023

--- adventofcode/_2023/day1/challenge.py
+++ adventofcode/_2023/day1/challenge.py
@@ -4,17 +4,18 @@ from adventofcode.utils import open_input
 def main():
     data = open_input('adventofcode/_2023/day1/input.txt')
     answer_1 = get_answer_1(data)
-    # answer_2 = get_answer_2(data)
+    answer_2 = get_answer_2(data)
 
     print(answer_1)
-    # print(answer_2)
+    print(answer_2)
 
-    return answer_1
+    return answer_1, answer_2
 
 
 def get_answer_1(data):
     """
     - get the first and last digit of each line, make a two digit number per line, sum all outcomes together
+    - This is my initial approach, a better approach is found in answer 2
     """
     sums = []
     for _, line in enumerate(data):
@@ -32,9 +33,29 @@ def get_answer_1(data):
         sums.append(int(newline))
 
     return sum(sums)
-        
 
 
+def get_answer_2(data):
+    """
+    - get the first and last digit of each line, make a two digit number per line, sum all outcomes together
+    - for part 2, there can be words spelled out as well
+    - Part 2 help: https://www.youtube.com/watch?v=rnidYOt9m2o
+    """
+    answer = 0
+    for _, line in enumerate(data):
+        digits = []
+        for char_index, char in enumerate(line):
+            if char.isdigit():
+                digits.append(char)
+            for digit, value in enumerate(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
+                if line[char_index:].startswith(value):
+                    # Cheating by using the index of the list of spelled out numbers as the stored digits plus one
+                    digits.append(str(digit + 1))
+
+        line_score = int(digits[0] + digits[-1])
+        answer += line_score
+
+    return answer
 
 
 if __name__ == '__main__':
--- test/unit/_2023/test_day1.py
+++ test/unit/_2023/test_day1.py
@@ -2,7 +2,7 @@ from adventofcode._2023.day1.challenge import main
 
 
 def test_input():
-    answer1 = main()
+    answer1, answer2 = main()
 
     assert answer1 == 55621
-    # assert answer2 == 123
+    assert answer2 == 53592
